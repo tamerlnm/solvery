8b9317e566de7443baf950ba80caadc7549881dd
tamerlan <tamerlan@connectedhome.kz>
2025-01-06 00:25:50 +0500

    refactoring unpackValue and packValue function for readable

--- pack_unpack/main.go
+++ pack_unpack/main.go
@@ -1,17 +1,14 @@
 package main
 
 import (
+	"coder/utils"
 	"flag"
 	"fmt"
-	"log"
 	"os"
 	"strconv"
 )
 
 func main() {
-	if len(os.Args) != 4 {
-		log.Fatal("Wrong data inputs")
-	}
 	var arg string
 	input := flag.Bool("input", false, "input argument")
 	daemon := flag.Bool("daemon", false, "daemon argument")
@@ -26,7 +23,7 @@ func main() {
 		fmt.Scanln(&arg)
 	}
 
-	if !isValidArgument(arg) {
+	if !utils.IsValidArgument(arg) {
 		fmt.Println(`""`)
 		return
 	}
@@ -40,72 +37,41 @@ func main() {
 }
 
 func unpackValue(arg string) string {
+	arg, _ = strconv.Unquote(`"` + arg + `"`)
 	result := make([]rune, 0)
-	for i := 0; i < len(arg); i++ {
-		if arg[i] >= 48 && arg[i] <= 57 {
-			n, err := strconv.Atoi(string(arg[i]))
-			if err != nil {
-				log.Fatal("error to convert string to integer")
-			}
-			if n == 0 && len(result) > 0 {
-				result = result[:len(result)-1]
-			} else {
-				for n > 1 {
-					if i > 1 && arg[i-2] == 92 {
-						result = append(result, rune(92), rune(arg[i-1]))
-					} else {
-						result = append(result, rune(arg[i-1]))
-					}
-					n--
-				}
-			}
-		} else {
-			result = append(result, rune(arg[i]))
+	prevRune := rune(0)
+	for _, r := range arg {
+		if r < '0' || r > '9' {
+			result = append(result, r)
+			prevRune = r
+			continue
 		}
+
+		n, _ := strconv.Atoi(string(r))
+		if n == 0 && len(result) > 0 {
+			result = result[:len(result)-1]
+		}
+		utils.AppendToSliceOfRune(&result, prevRune, n)
 	}
-	return string(result)
+	return utils.Format(string(result))
 }
 
 func packValue(arg string) string {
-	if len(arg) == 0 {
-		return ""
-	}
-
 	result := ""
 	count := 1
-	for i := 0; i < len(arg); i++ {
-		if i+1 < len(arg) && arg[i] == arg[i+1] {
+	runes := []rune(arg)
+
+	for i, r := range runes {
+		if i+1 < len(runes) && r == runes[i+1] {
 			count++
-		} else {
-			result += string(arg[i])
-			if count > 1 {
-				result += strconv.Itoa(count)
-			}
-			count = 1
+			continue
 		}
+
+		result += string(r)
+		if count > 1 {
+			result += strconv.Itoa(count)
+		}
+		count = 1
 	}
 	return result
-}
-
-func isValidArgument(s string) bool {
-	if s[0] >= 49 && s[0] <= 57 {
-		return false
-	}
-
-	for i := 1; i < len(s); i++ {
-		if s[i] >= 48 && s[i] <= 57 && s[i-1] >= 48 && s[i-1] <= 57 {
-			return false
-		}
-	}
-
-	count := 0
-	for i := 0; i < len(s); i++ {
-		if s[i] >= 49 && s[i] <= 57 {
-			count++
-		}
-	}
-	if count == len(s) {
-		return false
-	}
-	return true
 }
