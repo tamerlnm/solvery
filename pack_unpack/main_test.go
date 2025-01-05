4138867a3c4b12a455e68952af734ce6da1b4200
tamerlan <tamerlan@connectedhome.kz>
2025-01-06 00:22:35 +0500

    added new test cases

--- pack_unpack/main_test.go
+++ pack_unpack/main_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestUnpackValue(t *testing.T) {
 	tests := []struct {
@@ -11,7 +13,9 @@ func TestUnpackValue(t *testing.T) {
 		{"abcd", "abcd"},
 		{"aaa0b", "aab"},
 		{"adas4lopa5", "adasssslopaaaaa"},
-		{"d\n5abc", "d\n\n\n\n\nabc"},
+		{`d\n5abc`, `d\n\n\n\n\nabc`},
+		{`l2кац3\n4иb5`, `llкаццц\n\n\n\nиbbbbb`},
+		{"и3s2у3", "иииssууу"},
 	}
 	for _, test := range tests {
 		result := unpackValue(test.input)
@@ -30,6 +34,8 @@ func TestPackValue(t *testing.T) {
 		{"abcd", "abcd"},
 		{"aaaabbbcce", "a4b3c2e"},
 		{"aabkkllgggahjjhdi", "a2bk2l2g3ahj2hdi"},
+		{"аааабббвв", "а4б3в2"},
+		{"аааабббввttttt", "а4б3в2t5"},
 	}
 	for _, test := range tests {
 		result := packValue(test.input)
