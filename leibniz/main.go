34a102e525037d8473c8f6e2b768489fff2b9a6c
tamerlan <tamerlan@connectedhome.kz>
2025-02-16 11:57:21 +0500

    changed LeibnizFormula's logic

--- leibniz/main.go
+++ leibniz/main.go
@@ -11,7 +11,7 @@ import (
 
 func main() {
 	var sum float64
-	n := flag.Int("n", 2, "help message for flag n")
+	n := flag.Int("n", 3, "help message for flag n")
 	flag.Parse()
 	wg := sync.WaitGroup{}
 	results := make(chan float64, *n)
@@ -19,46 +19,41 @@ func main() {
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
 
-	wg.Add(*n)
-	for i := 0; i < *n; i++ {
-		go LeibnizFormula(i, 10000, *n, &wg, results, stop)
-	}
 	go func() {
 		<-sigChan
+		fmt.Println("\nProgram has stopped...")
 		close(stop)
 	}()
 
+	wg.Add(*n)
+	for i := 0; i < *n; i++ {
+		go LeibnizFormula(i, *n, &wg, results, stop)
+	}
+
 	wg.Wait()
 	close(results)
+
 	for r := range results {
 		sum += r
 	}
 	fmt.Println(sum)
 }
 
-func LeibnizFormula(id, terms, n int, wg *sync.WaitGroup, results chan float64, stop chan struct{}) {
+func LeibnizFormula(id, n int, wg *sync.WaitGroup, results chan float64, stop chan struct{}) {
 	defer wg.Done()
 	sum := 0.0
-	sign := 1.0
 
-	if id%2 != 0 {
-		sign = -1.0
-	}
-
-	//for i := id; i < terms; i += n {
-	//	sum += sign / float64(2*i+1)
-	//	sign = -sign
-	//}
-
-	for i := id; i < terms; i += n {
+	for i := id; ; i += n {
 		select {
 		case <-stop:
 			results <- sum * 4
 			return
 		default:
+			sign := 1.0
+			if i%2 != 0 {
+				sign = -1.0
+			}
 			sum += sign / float64(2*i+1)
-			sign = -sign
 		}
 	}
-	results <- sum * 4
 }
